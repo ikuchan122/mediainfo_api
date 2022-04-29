dbbeed56a1f447b947c95c3fa3f33505808b6a4a
ikuchan122 <67225553+ikuchan122@users.noreply.github.com>
2022-04-30 06:14:27 +0800

    bug fix for URL validator

--- mediainfo_api/views.py
+++ mediainfo_api/views.py
@@ -15,24 +15,32 @@ from mediainfo_api.validator import valid_url
 @require_http_methods(["GET", "POST"])
 @csrf_exempt
 def getVideoDimensions(request):
+    # POST
+    if request.method == 'POST':
+        video = request.FILES['file']
+
+    # GET
     if request.method == 'GET':
-        query_str = request.GET
-        if not 'url' in query_str:
+        # validate query string / url param
+        query_dict = request.GET
+        if not 'url' in query_dict:
             return JsonResponse({'ok': False, 'message': 'Enter a valid parameter.'})
-        url = query_str['url']
+
+        # extract video url from query string
+        query_str = query_dict.urlencode(safe=':/?&=')
+        url = query_str[query_str.index('url=')+4:]
         if not valid_url(url):
             return JsonResponse({'ok': False, 'message': 'Enter a valid URL.'})
+
+        # fetch video
         try:
             video = BytesIO(requests.get(url, timeout=(3, 10)).content)
         except Timeout:
             return JsonResponse({'ok': False, 'message': 'Connection timeout when downloading the video.'})
 
-    if request.method == 'POST':
-        video = request.FILES['file']
-
+    # call Mediainfo.parse()
     print('request received!')
     start = time.time()
-
     tracks = MediaInfo.parse(video).video_tracks
     height, width, duration = 0, 0, 0
     for track in tracks:
@@ -40,8 +48,9 @@ def getVideoDimensions(request):
             height = track.height
             width = track.width
             duration = ceil(track.duration / 1000)
-
     end = time.time()
+
+    # return json response
     if height == 0 or width == 0:
         return JsonResponse({'ok': False, 'message': 'Enter a valid URL to a video file.'})
 
